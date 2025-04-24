72177423281cd3ffa2361e05f0998fa750a00432
louieBuera <isaac.277.elifer@gmail.com>
2025-04-25 07:53:51 +0800

    Bottom Navigation Bar

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -9,7 +9,6 @@ export default function Index() {
       <Text className="text-5xl text-dark-200  font-bold">
         Hello World!
       </Text>
-      <Link className="text-2xl" href="/onboarding">Onboarding</Link>
       <Link className="text-2xl" href="/profile">Profile</Link>
       <Link className="text-2xl" href="/saved">Saved</Link>
       <Link className="text-2xl" href="/search">Search</Link>

8c04f1ff2e3c79cba39929a8a3d7a52317377aa7
louieBuera <isaac.277.elifer@gmail.com>
2025-04-25 12:16:48 +0800

    Add API service + hook WIP

---
+++ services/useFetch.ts
@@ -0,0 +1,36 @@
+import { useEffect, useState } from "react";
+
+export function useFetch<T>(
+  fetchFunction: () => Promise<T>,
+  autoFetch = true
+) {
+  const [data, setData] = useState<T|null>(null);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState<Error | null>(null);
+
+  const fetchData = async () => {
+    try {
+      setLoading(true);
+      setError(null);
+      const result = await fetchFunction();
+
+      setData(result);
+    } catch (err) {
+      setError(err instanceof Error ? err : new Error('An Error Occurred'));
+    } finally {
+      setLoading(false);
+    }
+  }
+
+  const reset = () => {
+    setData(null);
+    setLoading(false);
+    setError(null);
+  }
+
+  useEffect(() => {
+    if(autoFetch) fetchData()
+  }, []);
+
+  return { data, loading, error, refetch: fetchData, reset }
+}

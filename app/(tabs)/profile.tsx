2d3080a4ffeca99e011f298e80d618e9e55c1f6f
louieBuera <isaac.277.elifer@gmail.com>
2025-04-26 21:59:58 +0800

    Put Nicer Stubs for Profile + Saved Screens

--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -1,12 +1,19 @@
+import { icons } from '@/constants/icons';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
 
 const Profile = () => {
-    return (
-        <View>
-            <Text>Profile</Text>
-        </View>
-    )
+	return (
+		<View className='bg-primary flex-1 px-10'>
+			<View className="flex justify-center items-center flex-1 flex-col gap-5">
+				<Image source={icons.person}
+					className="size-10"
+					tintColor="#FFF"
+				/>
+				<Text className="text-gray-500 text-base">Profile</Text>
+			</View>
+		</View>
+	)
 }
 
 export default Profile;

f3bbf3e2f7acefdaa60dad2b4f1481bf769f92c9
louieBuera <isaac.277.elifer@gmail.com>
2025-04-26 21:54:08 +0800

    Movie Details

--- services/api.ts
+++ services/api.ts
@@ -21,10 +21,31 @@ export const fetchMovies = async ({ query }: {
 
   if(!response.ok){
     // @ts-ignore
-    throw new Error('Failed to fetch movie: ', response.statusText);
+    throw new Error('Failed to fetch movies: ', response.statusText);
 
   }
 
   const data = await response.json();
   return data.results;
+}
+
+export const fetchMovieDetails = async (movieId: string): Promise<MovieDetails> => {
+  try {
+    const response = await fetch(`${TMDB_CONFIG.BASE_URL}/movie/${movieId}?api_key=${TMDB_CONFIG.API_KEY}`, {
+      method: 'GET',
+      headers: TMDB_CONFIG.headers
+    });
+
+    if(!response.ok){
+      // @ts-ignore
+      throw new Error('Failed to fetch movie details: ', response.statusText);
+  
+    }
+
+    const data = await response.json();
+    return data;
+  } catch (err) {
+    console.log(err);
+    throw err;
+  }
 }
